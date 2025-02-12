e656e0c68a387d2803bef66439ed2bbb97235581
robinv312 <robinv312@gmail.com>
2025-02-12 22:30:07 +0530

    Initial commit

--- src/components/Grocery.js
+++ src/components/Grocery.js
@@ -1,8 +1,25 @@
+import {useState}from "react"
 const Grocery=()=>{
-    return(
-        <div>
-            <h1>its look diffrent</h1>
-        </div>
-    )
-};
+  const [formData, setFormData] = useState({username:""});
+  const handlechange=(e)=>{
+    setFormData({[e.target.name]: e.target.value });
+    console.log(e.target.value)
+  }
+
+  return(
+    <div>
+      <form>
+        <h1>login</h1>
+        <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={formData.username}
+            onChange={handlechange}
+            className="w-full p-2 border border-gray-300 rounded"
+          />
+      </form>
+    </div>
+  ) 
+}
 export default Grocery;
